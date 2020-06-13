9039fb3bdaf117e79454faede73820c490a26d91
s20500-pj <56227312+s20500-pj@users.noreply.github.com>
2020-06-13 10:36:47 +0200

    usuniecie smieci

--- projektJavaZaliczenie/myApplication.java
+++ projektJavaZaliczenie/myApplication.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -26,7 +25,7 @@ public class myApplication extends Application {
         TextArea inputField = new TextArea();
         TextArea histogramArea = new TextArea();
         inputField.setPromptText("Tutaj wpisz tekst do dokonania histogramu");
-        inputField.setFocusTraversable(false);
+        inputField.setFocusTraversable(false); //focus
         histogramArea.setPromptText("Tutaj pojawi się twój histogram");
         //Buttons
         Button button1 = new Button();
